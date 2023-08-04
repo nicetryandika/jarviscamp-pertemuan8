7b91c26cbfe44aab405e70056af3bceb208cdb92
Andika Hari Wijaya <hariwijayaandika@gmail.com>
2023-08-04 23:35:08 +0700

    menambahkan proyek laravel blogs-apps

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $data = [
+        "title" => "Dashboard",
+        "user" => [
+            "name" => "<h5>Hallo, Andika</h5>",
+            "email" => "hariwijayaandika@gmail.com",
+            //"pesan" => "<h1>HALLOOOOO</h1>"
+        ]
+    ];
+    return view('contents.dashboard', $data);
+});
+
+Route::get('/users', function () {
+    return view('contents.users', [
+        "title" => "Users"
+    ]);
+});
